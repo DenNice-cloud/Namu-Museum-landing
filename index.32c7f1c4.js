1e589039535845f83e82d512ecae2fee01af2098
Denis Kotov <dionisiy2904@gmail.com>
2024-06-02 11:35:52 +0300

    make build

---
+++ index.32c7f1c4.js
@@ -0,0 +1,2 @@
+var e,i=document.querySelector(".swiper");function t(){window.innerWidth<=1024&&"false"===i.dataset.slider&&(e=new Swiper(".swiper",{slidesPerView:1,spaceBetween:15,centeredSlides:!0,pagination:{el:".swiper-pagination",clickable:!0},width:280,breakpoints:{640:{slidesPerView:1,spaceBetween:20,width:340}}}),i.dataset.slider="true"),window.innerWidth>1024&&(i.dataset.slider="false",i.classList.contains("swiper-initialized")&&e.destroy())}t(),window.addEventListener("resize",function(){return t()}),e.slideNext();
+//# sourceMappingURL=index.32c7f1c4.js.map
